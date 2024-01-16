179e9a51d5eff14075e39e8b07ae62d8afe3ee07
Arielb11 <arielbuchhamer1@outlook.com>
2024-01-16 15:49:49 -0300

    Agregado componente home, phone y agregarPhone al front

---
+++ Front/app-tecnophones/src/app/components/phone/phone.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-phone',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './phone.component.html',
+  styleUrl: './phone.component.css'
+})
+export class PhoneComponent {
+
+}
---
+++ Front/app-tecnophones/src/app/models/phone.ts
@@ -0,0 +1,3 @@
+export class Phone {
+    constructor (public modelo?:string, public estado?:string, public bateria?:number, public capacidad?:number, public observaciones?:string, public valor?:number) {}
+}
