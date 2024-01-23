50335a6c6dce10c761e214862c345aa261eee589
Juan Cruz Romero Albornoz <cancu.romero@gmail.com>
2024-01-23 11:57:30 -0300

    actualizacion Front
    ya ni me acuerdo que hice

--- Front/app-tecnophones/src/app/app.routes.ts
+++ Front/app-tecnophones/src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { PhoneComponent } from './components/phone/phone.component';
 import { HomeComponent } from './components/home/home.component';
 import { AgregarPhoneComponent } from './components/agregar-phone/agregar-phone.component';
 import { LoginComponent } from './components/login/login.component';
+import { PhoneNuevosComponent } from './components/phone-nuevos/phone-nuevos.component';
+import { PhoneUsadosComponent } from './components/phone-usados/phone-usados.component';
+import { AccesoriosComponent } from './components/accesorios/accesorios.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'phone', component: PhoneComponent},
-    {path: 'agregar-phone', component: AgregarPhoneComponent},
-    {path: 'editar-phone/:id', component: AgregarPhoneComponent},
-    {path: 'login', component: LoginComponent},
-    {path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: '', component: HomeComponent },
+  { path: 'phone', component: PhoneComponent },
+  { path: 'agregar-phone', component: AgregarPhoneComponent },
+  { path: 'editar-phone/:id', component: AgregarPhoneComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'phone-nuevos', component: PhoneNuevosComponent },
+  { path: 'phone-usados', component: PhoneUsadosComponent },
+  { path: 'accesorios', component: AccesoriosComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ Front/app-tecnophones/src/app/components/phone-usados/phone-usados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-usados',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-usados.component.html',
+  styleUrl: './phone-usados.component.css'
+})
+export class PhoneUsadosComponent {
+
+}
