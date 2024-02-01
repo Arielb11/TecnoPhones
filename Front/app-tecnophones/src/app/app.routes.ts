26d2283952f1626a5d64051e878e3c25947eae8a
Arielb11 <arielbuchhamer1@outlook.com>
2024-02-01 11:01:14 -0300

    Botones de home funcionando

--- Front/app-tecnophones/src/app/app.routes.ts
+++ Front/app-tecnophones/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { HomeComponent } from './components/home/home.component';
 import { AgregarPhoneComponent } from './components/agregar-phone/agregar-phone.component';
 import { LoginComponent } from './components/login/login.component';
 import { VistaPhonesComponent } from './components/vista-phones/vista-phones.component';
+import { PhoneNuevosComponent } from './components/phone-nuevos/phone-nuevos.component';
+import { PhoneUsadosComponent } from './components/phone-usados/phone-usados.component';
 import { loginGuard } from './guards/login.guard';
 
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'phones', component: VistaPhonesComponent},
+    {path: 'iPhoneNuevos', component: PhoneNuevosComponent},
+    {path: 'iPhoneUsados', component: PhoneUsadosComponent},
     {path: 'phone', component: PhoneComponent, canActivate: [loginGuard]},
     {path: 'agregar-phone', component: AgregarPhoneComponent, canActivate: [loginGuard]},
     {path: 'editar-phone/:id', component: AgregarPhoneComponent, canActivate: [loginGuard]},
