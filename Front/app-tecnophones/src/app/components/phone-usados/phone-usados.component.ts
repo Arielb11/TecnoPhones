6a7054d81c12ef1243f74e88a3a498aeb25ec8ec
Juan Cruz Romero Albornoz <93006752+CancuRomero@users.noreply.github.com>
2024-02-06 21:50:20 -0300

    actualizacion-front-vistaUsados
    nashe

--- Front/app-tecnophones/src/app/components/phone-usados/phone-usados.component.ts
+++ Front/app-tecnophones/src/app/components/phone-usados/phone-usados.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhoneService } from '../../services/phone.service';
+import { Phone } from '../../models/phone';
+import { error } from 'console';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-phone-usados',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './phone-usados.component.html',
   styleUrl: './phone-usados.component.css'
 })
-export class PhoneUsadosComponent {
+export class PhoneUsadosComponent implements OnInit {
+  listaCelulares:Phone[]=[];
+  ngOnInit(): void {
+    this.obtenerPhones();
+  } 
+    constructor(private _phoneService:PhoneService){}
 
+    obtenerPhones(){
+      this._phoneService.getPhones().subscribe(data =>{
+        this.listaCelulares= data;
+        
+      },error =>{console.log(error)});
+    }
 }
