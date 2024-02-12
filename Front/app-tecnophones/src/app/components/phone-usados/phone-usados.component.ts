0c7d1088dcb3ca70a496625c8acbdda9aef09394
Juan Cruz Romero Albornoz <93006752+CancuRomero@users.noreply.github.com>
2024-02-12 19:20:20 -0300

    Actualizacion front
    se agrego el NAV

--- Front/app-tecnophones/src/app/components/phone-usados/phone-usados.component.ts
+++ Front/app-tecnophones/src/app/components/phone-usados/phone-usados.component.ts
@@ -3,11 +3,14 @@ import { PhoneService } from '../../services/phone.service';
 import { Phone } from '../../models/phone';
 import { error } from 'console';
 import { CommonModule } from '@angular/common';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+
+
 
 @Component({
   selector: 'app-phone-usados',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MdbCheckboxModule],
   templateUrl: './phone-usados.component.html',
   styleUrl: './phone-usados.component.css'
 })
